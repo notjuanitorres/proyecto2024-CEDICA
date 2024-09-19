c52061275cda9434d81134580bc88df2c3f65937
daniNKV <nikiforovdaniel@gmail.com>
2024-09-19 04:56:01 -0300

    register routes and commands outside the entrypoint

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from src.web.config import config
-from src.web.handlers import error
-from src.web.controllers.index import index_bp
-from src.web.controllers.user import users_bp
-from src.web.controllers.auth import auth_bp
-from src.core import database
 from flask_session import Session
-from src.core.bcrypt import bcrypt
 from web.helpers.auth import is_authenticated
+from src.core.config import config
+from src.core import database
+from src.core.bcrypt import bcrypt
 from src.core.wiring import init_wiring
+from src.web.handlers import error
+from src.web.routes import register_blueprints
+from src.web.commands import register_commands
 
 session = Session()
 
@@ -18,22 +17,17 @@ def create_app(env="development", static_folder="../../static"):
 
     app.config.from_object(config[env])
 
-    #extensions
     database.init_app(app)
     session.init_app(app)
     bcrypt.init_app(app)
-    app.register_blueprint(index_bp)
-    app.register_blueprint(users_bp)
-    app.register_blueprint(auth_bp)
+
+    register_blueprints(app)
+    register_commands(app)
     init_wiring()
 
     error_codes = [400, 401, 403, 404, 405, 500]
     for code in error_codes:
         app.register_error_handler(code, error.handle_error)
-
-    @app.cli.command(name="reset-db")
-    def reset_db():
-        database.reset(app)
 
     app.jinja_env.globals.update(is_authenticated=is_authenticated)
 
--- admin/src/web/controllers/__init__.py
+++ admin/src/web/controllers/__init__.py
@@ -1,1 +1,4 @@
 from . import auth, user
+from .index import index_bp
+from .auth import auth_bp
+from .user import users_bp
