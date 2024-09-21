f5e56e1cd7ff9d17e630f95e39ae4f66d32f7db4
daniNKV <nikiforovdaniel@gmail.com>
2024-09-21 14:56:18 -0300

    correcting commands location

--- admin/src/core/commands.py
+++ admin/src/core/commands.py
@@ -1,7 +1,12 @@
 from src.core import database
+from src.core.seeds import seed_accounts
 
 
 def register_commands(app):
     @app.cli.command(name="reset-db")
     def reset_db():
         database.reset(app)
+    
+    @app.cli.command(name="seed-db")
+    def seed_db():
+        seed_accounts()
