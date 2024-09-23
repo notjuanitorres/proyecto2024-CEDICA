f71e37498475a3ffe7c39e20a3c2c658681e9619
daniNKV <nikiforovdaniel@gmail.com>
2024-09-23 05:08:31 -0300

    refactor: extract email validation logic from the controllers

--- admin/src/core/module/accounts/forms.py
+++ admin/src/core/module/accounts/forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional
-
+from .validators import EmailExistence
 
 class UserCreateForm(FlaskForm):
     email = StringField(
         "Email",
-        validators=[DataRequired(), Email(
-            message="Email invalido"), Length(max=100)],
+        validators=[
+            DataRequired(),
+            Email(message="Email invalido"),
+            EmailExistence("El email se encuentra en uso"),
+            Length(max=100),
+        ],
     )
 
     password = PasswordField(
@@ -46,13 +50,27 @@ class UserCreateForm(FlaskForm):
     enabled = BooleanField("Enabled", default=True)
 
     system_admin = BooleanField("System Admin", default=False)
+    current_email = None
 
+
+def email_existence(form, field):
+    validator = EmailExistence("Email en uso")
+    validator(form, field)
 
 class UserEditForm(FlaskForm):
+    def __init__(self, *args, **kwargs):
+        self.current_email = kwargs.pop('current_email', None)
+        super(UserEditForm, self).__init__(*args, **kwargs)
+
+
     email = StringField(
         "Email",
-        validators=[DataRequired(), Email(
-            message="Email invalido"), Length(max=100)],
+        validators=[
+            DataRequired(),
+            Email(message="Email invalido"),
+            email_existence,
+            Length(max=100)
+        ],
     )
     role_id = SelectField(
         "Role",
