fe114feff882a5034774b59932d579c5ae594da2
daniNKV <nikiforovdaniel@gmail.com>
2024-09-30 02:43:12 -0300

    fix: correct missing attribute after merge

--- admin/src/core/module/employee/models.py
+++ admin/src/core/module/employee/models.py
@@ -10,6 +10,7 @@ class Employee(db.Model, AddressMixin, EmergencyContactMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), unique=True)
     name = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
     fullname = column_property(name + " " + lastname)
     dni = db.Column(db.Integer, nullable=False, unique=True)
