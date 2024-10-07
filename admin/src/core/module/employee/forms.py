9a2c1dae24ed7a9cf7dde8f2dc4338b7a69ee344
daniNKV <nikiforovdaniel@gmail.com>
2024-10-07 01:58:05 -0300

    allow tu submit the employee create form validating submitted files

--- admin/src/core/module/employee/forms.py
+++ admin/src/core/module/employee/forms.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, Email, Length, Optional
+from flask_wtf.file import FileAllowed, FileSize, FileRequired
+from wtforms.validators import DataRequired, Email, Length, Optional, ValidationError
 from wtforms.fields import (
     StringField,
     BooleanField,
@@ -9,6 +10,8 @@ from wtforms.fields import (
     DateField,
     TextAreaField,
     SubmitField,
+    FileField,
+    MultipleFileField,
 )
 from src.core.module.employee.data import ProfessionsEnum, PositionEnum, ConditionEnum
 from src.core.module.employee.validators import EmailExistence, DniExistence
@@ -50,6 +53,78 @@ class EmploymentInformationForm(FlaskForm):
     is_active = BooleanField("Activo en la organizacion")
 
 
+def max_file_size(size_in_mb: int):
+    BYTES_PER_MB = 1024 * 1024
+
+    size_in_bytes = size_in_mb * BYTES_PER_MB
+
+    return size_in_bytes
+
+
+class FilesNumber(object):
+    def __init__(self, min=-1, max=-1, message=None):
+        self.min = min
+        self.max = max
+        if not message:
+            message = f"Maximum number of files is {max} and the minimum {min}." % (
+                min,
+                max,
+            )
+        self.message = message
+
+    def __call__(self, form, field):
+        files = field.data
+
+        if (self.min != -1 and len(files) < self.min) or (
+            self.max != -1 and len(files) > self.max
+        ):
+            raise ValidationError(self.message)
+
+
+class EmployeeDocumentsForm(FlaskForm):
+    dni = MultipleFileField(
+        validators=[
+            # FileRequired(),
+            FileSize(
+                max_size=max_file_size(size_in_mb=5),
+                message="El archivo es demasiado grande",
+            ),
+            FileAllowed(
+                ["pdf", "jpg", "jpeg", "png"],
+                message="Formato no reconocido. Formato valido: .pdf, .jpg, .jpeg, .png",
+            ),
+            FilesNumber(min=0, max=2, message="Puede subir hasta 2 archivos"),
+        ]
+    )
+    title = MultipleFileField(
+        validators=[
+            # FileRequired(),
+            FileSize(
+                max_size=max_file_size(size_in_mb=5),
+                message="El archivo es demasiado grande",
+            ),
+            FileAllowed(
+                ["pdf", "jpg", "jpeg", "png"],
+                message="Formato no reconocido Formato valido: .pdf, .jpg, .jpeg, .png",
+            ),
+            FilesNumber(min=0, max=5, message="Puede subir hasta 5 archivos"),
+        ]
+    )
+    curriculum_vitae = FileField(
+        validators=[
+            # FileRequired(),
+            FileSize(
+                max_size=max_file_size(size_in_mb=5),
+                message="El archivo es demasiado grande",
+            ),
+            FileAllowed(
+                upload_set=["pdf", "jpg", "jpeg"],
+                message="Formato no reconocido. Formato valido: .pdf",
+            ),
+        ]
+    )
+
+
 class EmployeeManagementForm(FlaskForm):
     def __init__(self, *args, **kwargs):
         super(EmployeeManagementForm, self).__init__(*args, **kwargs)
@@ -64,8 +139,9 @@ class EmployeeManagementForm(FlaskForm):
     health_insurance = TextAreaField("Obra Social", validators=[Optional()])
     affiliate_number = StringField("Numero de afiliado", validators=[Optional()])
     emergency_contact = FormField(EmergencyContactForm)
+    documents = FormField(EmployeeDocumentsForm)
 
-    # user_id = IntegerField("", validators=[DataRequired()])
+    # TODO: Find a way to relationate an account's email or id
 
 
 class EmployeeCreateForm(EmployeeManagementForm):
