9cdd33129d56aea376f0ae706391d7e53203c59c
Matgc <gomezcarriquematias@gmail.com>
2024-10-19 12:58:20 -0300

    refactor: add docstrings and remove useless code in horse module

--- admin/src/core/module/equestrian/models.py
+++ admin/src/core/module/equestrian/models.py
@@ -5,6 +5,16 @@ from enum import Enum as pyEnum
 
 
 class JAEnum(pyEnum):
+    """
+    Enumeration for different types of J&A activities.
+
+    Attributes:
+        HIPOTHERAPY (str): Hipoterapia.
+        THERAPEUTIC_RIDING (str): Monta Terapéutica.
+        ADAPTED_SPORTS (str): Deporte Ecuestre Adaptado.
+        RECREATIONAL_ACTIVITIES (str): Actividades Recreativas.
+        RIDING (str): Equitación.
+    """
     HIPOTHERAPY = "Hipoterapia"
     THERAPEUTIC_RIDING = "Monta Terapéutica"
     ADAPTED_SPORTS = "Deporte Ecuestre Adaptado"
@@ -13,6 +23,25 @@ class JAEnum(pyEnum):
 
 
 class Horse(db.Model):
+    """
+    Model representing a horse.
+
+    Attributes:
+        id (int): The unique identifier of the horse.
+        name (str): The name of the horse.
+        birth_date (datetime.date): The birth date of the horse.
+        sex (str): The sex of the horse.
+        breed (str): The breed of the horse.
+        coat (str): The coat color of the horse.
+        is_donation (bool): Indicates if the horse is a donation.
+        admission_date (datetime.date): The admission date of the horse.
+        assigned_facility (str): The facility assigned to the horse.
+        ja_type (JAEnum): The type of J&A assigned to the horse.
+        files (List[HorseFile]): The list of files related to the horse.
+        is_deleted (bool): Indicates if the horse is deleted.
+        inserted_at (datetime.datetime): The timestamp when the horse was inserted.
+        updated_at (datetime.datetime): The timestamp when the horse was last updated.
+    """
     __tablename__ = 'horses'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -34,6 +63,14 @@ class Horse(db.Model):
 
 
 class HorseTrainers(db.Model):
+    """
+    Model representing the relationship between horses and trainers.
+
+    Attributes:
+        id (int): The unique identifier of the relationship.
+        id_horse (int): The unique identifier of the horse.
+        id_employee (int): The unique identifier of the employee (trainer).
+    """
     __tablename__ = 'horse_trainers'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +79,16 @@ class HorseTrainers(db.Model):
 
 
 class FileTagEnum(pyEnum):
+    """
+    Enumeration for different types of file tags.
+
+    Attributes:
+        FICHA_GENERAL (str): Ficha general del caballo.
+        PLANIFICACION_ENTRENAMIENTO (str): Planificación de Entrenamiento.
+        INFORME_EVOLUCION (str): Informe de Evolución.
+        CARGA_IMAGENES (str): Carga de Imágenes.
+        REGISTRO_VETERINARIO (str): Registro veterinario.
+    """
     FICHA_GENERAL = "Ficha general del caballo"
     PLANIFICACION_ENTRENAMIENTO = "Planificación de Entrenamiento"
     INFORME_EVOLUCION = "Informe de Evolución"
@@ -50,6 +97,13 @@ class FileTagEnum(pyEnum):
 
 
 class HorseFile(File):
+    """
+    Model representing a file related to a horse.
+
+    Attributes:
+        horse_id (int): The unique identifier of the horse.
+        owner (Horse): The horse that owns the file.
+    """
     __mapper_args__ = {
         "polymorphic_identity": "horse",
     }
