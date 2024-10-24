be9715ab0012e2e7b630d69fa9fa8e24c41babb4
Juan Ignacio Torres <torresjuanilp@gmail.com>
2024-10-24 13:49:39 -0300

    refactor: add docstrings to auth module

--- admin/src/core/module/auth/services.py
+++ admin/src/core/module/auth/services.py
@@ -7,45 +7,139 @@ from .models import Role
 
 
 class AbstractAuthServices:
+    """
+    Abstract base class for authentication services.
+
+    This class defines the interface for authentication services, including methods
+    for authenticating users, validating emails, retrieving roles and permissions,
+    and checking user permissions.
+    """
+
     @abstractmethod
     def authenticate(self, email: str, password: str) -> Dict | None:
+        """
+        Authenticate a user by their email and password.
+
+        Args:
+            email (str): The email address of the user.
+            password (str): The password of the user.
+
+        Returns:
+            Dict | None: The authenticated user data as a dictionary, or None if authentication fails.
+        """
         pass
 
     @abstractmethod
     def validate_email(self, email: str) -> bool:
+        """
+        Validate if an email is already in use.
+
+        Args:
+            email (str): The email address to validate.
+
+        Returns:
+            bool: True if the email is in use, False otherwise.
+        """
         pass
 
     @abstractmethod
     def get_role(self, role_id: int) -> Role:
+        """
+        Retrieve a role by its ID.
+
+        Args:
+            role_id (int): The ID of the role to retrieve.
+
+        Returns:
+            Role: The role with the specified ID.
+        """
         pass
 
     @abstractmethod
-    def get_roles(self) -> List:
+    def get_roles(self) -> List[Role]:
+        """
+        Retrieve all roles.
+
+        Returns:
+            List[Role]: A list of all roles.
+        """
         pass
 
     @abstractmethod
-    def get_permissions_of(self, user_id: int) -> List:
+    def get_permissions_of(self, user_id: int) -> List[str]:
+        """
+        Retrieve the permissions associated with a user.
+
+        Args:
+            user_id (int): The ID of the user whose permissions are to be retrieved.
+
+        Returns:
+            List[str]: A list of permissions associated with the specified user.
+        """
         pass
 
     @abstractmethod
-    def has_permissions(self, user_id: int, permissions_required: list[str]) -> bool:
+    def has_permissions(self, user_id: int, permissions_required: List[str]) -> bool:
+        """
+        Check if a user has the required permissions.
+
+        Args:
+            user_id (int): The ID of the user to check.
+            permissions_required (List[str]): A list of required permissions.
+
+        Returns:
+            bool: True if the user has the required permissions, False otherwise.
+        """
         pass
 
 
 class AuthServices(AbstractAuthServices):
+    """
+    Concrete implementation of the AbstractAuthServices.
+
+    This class provides methods for authenticating users, validating emails, retrieving roles and permissions,
+    and checking user permissions.
+    """
+
     def __init__(
         self,
         auth_repository: AbstractAuthRepository,
         user_repository: AbstractUserRepository,
     ):
+        """
+        Initialize the AuthServices.
+
+        Args:
+            auth_repository (AbstractAuthRepository): The repository for authentication data.
+            user_repository (AbstractUserRepository): The repository for user data.
+        """
         self.auth_repository = auth_repository
         self.user_repository = user_repository
 
     def validate_email(self, email: str) -> bool:
+        """
+        Validate if an email is already in use.
+
+        Args:
+            email (str): The email address to validate.
+
+        Returns:
+            bool: True if the email is in use, False otherwise.
+        """
         email_exists = self.user_repository.get_by_email(email) is not None
         return email_exists
 
     def authenticate(self, email: str, password: str):
+        """
+        Authenticate a user by their email and password.
+
+        Args:
+            email (str): The email address of the user.
+            password (str): The password of the user.
+
+        Returns:
+            Dict | None: The authenticated user data as a dictionary, or None if authentication fails.
+        """
         user = self.user_repository.get_by_email(email)
         if user is None or not user.enabled:
             return None
@@ -55,19 +149,53 @@ class AuthServices(AbstractAuthServices):
         return UserMapper.from_entity(user)
 
     def get_role(self, role_id: int) -> Role:
+        """
+        Retrieve a role by its ID.
+
+        Args:
+            role_id (int): The ID of the role to retrieve.
+
+        Returns:
+            Role: The role with the specified ID.
+        """
         return self.auth_repository.get_role(role_id)
 
-    def get_roles(self) -> List:
+    def get_roles(self) -> List[Role]:
+        """
+        Retrieve all roles.
+
+        Returns:
+            List[Role]: A list of all roles.
+        """
         return self.auth_repository.get_roles()
 
-    def get_permissions_of(self, user_id: int) -> List:
+    def get_permissions_of(self, user_id: int) -> List[str]:
+        """
+        Retrieve the permissions associated with a user.
+
+        Args:
+            user_id (int): The ID of the user whose permissions are to be retrieved.
+
+        Returns:
+            List[str]: A list of permissions associated with the specified user.
+        """
         user = self.user_repository.get_by_id(user_id)
         if not user:
             return ["NO_PERMISSIONS"]
         permissions = self.auth_repository.get_permissions_of_role(user.role_id)
         return [permission.name for permission in permissions]
 
-    def has_permissions(self, user_id: int, permissions_required: list[str]) -> bool:
+    def has_permissions(self, user_id: int, permissions_required: List[str]) -> bool:
+        """
+        Check if a user has the required permissions.
+
+        Args:
+            user_id (int): The ID of the user to check.
+            permissions_required (List[str]): A list of required permissions.
+
+        Returns:
+            bool: True if the user has the required permissions, False otherwise.
+        """
         if self.user_repository.is_sys_admin(user_id):
             return True
         if not self.user_repository.is_user_enabled(user_id):
--- admin/src/web/controllers/auth.py
+++ admin/src/web/controllers/auth.py
@@ -13,6 +13,16 @@ auth_bp = Blueprint(
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Display the login form and handle login requests.
+
+    If the user is already authenticated, redirect to the home page.
+    If the request method is POST, authenticate the user.
+    Otherwise, render the login form.
+
+    Returns:
+        A rendered template for the login form or a redirect to the home page.
+    """
     if is_authenticated(session):
         return redirect(url_for("index_bp.home"))
 
@@ -26,6 +36,17 @@ def login():
 
 @inject
 def authenticate(login_form: UserLoginForm, auth_services: AAS = Provide[Container.auth_services]):
+    """
+    Authenticate the user based on the login form data.
+
+    Args:
+        login_form (UserLoginForm): The form containing the user's login data.
+        auth_services (AAS): The authentication services.
+
+    Returns:
+        A rendered template for the login form if validation fails or authentication fails,
+        or a redirect to the home page if authentication is successful.
+    """
     if not login_form.validate_on_submit():
         return render_template("login.html", form=login_form)
 
@@ -51,6 +72,15 @@ def authenticate(login_form: UserLoginForm, auth_services: AAS = Provide[Contain
 
 @auth_bp.post("/logout")
 def logout():
+    """
+    Log out the user by clearing the session.
+
+    If the user is logged in, clear the session and flash a success message.
+    Redirect to the home page.
+
+    Returns:
+        A redirect to the home page.
+    """
     if session.get("user"):
         del session["user"]
         session.clear()
@@ -61,6 +91,16 @@ def logout():
 
 @auth_bp.route("/registrarse", methods=["GET", "POST"])
 def register():
+    """
+    Display the registration form and handle registration requests.
+
+    If the user is already authenticated, flash an info message and redirect to the home page.
+    If the request method is POST, register the user.
+    Otherwise, render the registration form.
+
+    Returns:
+        A rendered template for the registration form or a redirect to the home page.
+    """
     if is_authenticated(session):
         flash("Ya estás autenticado", "info")
         return redirect(url_for("index_bp.home"))
@@ -75,6 +115,17 @@ def register():
 
 @inject
 def register_user(registration_form: UserRegisterForm, user_repository: AAS = Provide[Container.user_repository]):
+    """
+    Register a new user based on the registration form data.
+
+    Args:
+        registration_form (UserRegisterForm): The form containing the user's registration data.
+        user_repository (AAS): The user repository services.
+
+    Returns:
+        A rendered template for the registration form if validation fails,
+        or a redirect to the home page if registration is successful.
+    """
     if not registration_form.validate_on_submit():
         return render_template("register.html", form=registration_form)
 
