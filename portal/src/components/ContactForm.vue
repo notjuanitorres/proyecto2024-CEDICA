680c6973cca9c2fc9a825550874249551eafda19
daniNKV <nikiforovdaniel@gmail.com>
2024-11-18 12:53:51 -0300

    create landing structure

---
+++ portal/src/components/ContactForm.vue
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<template>
+    <h1 class="title has-text-centered">Contactanos</h1>
+    <section id="about-us" class="container section is-medium has-background-white">
+
+    </section>
+</template>
